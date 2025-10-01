5391b76c8cd62d86508b69030a38c99c6196e154
Viktor <victorharasymiv97@gmail.com>
2025-10-01 19:24:36 +0200

    New update 19:21 01.10 correct

--- apps/frontend/src/services/reviews.js
+++ apps/frontend/src/services/reviews.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { API_URL } from "../../../../../plusfit.club.dashboard/src/config/api";
+import { API_URL } from "../../../../../plusfit.club.dashboard/src/config/api.js";
 
 export const ALL_REVIEWS = async (params = {}) => {
   try {

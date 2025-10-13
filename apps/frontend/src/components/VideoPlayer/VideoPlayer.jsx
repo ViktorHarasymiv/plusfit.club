6e594fcc060ec0061d15403f7653e588a910f5d9
Viktor <victorharasymiv97@gmail.com>
2025-10-13 22:49:37 +0200

    New update 22:45

--- apps/frontend/src/components/VideoPlayer/VideoPlayer.jsx
+++ apps/frontend/src/components/VideoPlayer/VideoPlayer.jsx
@@ -3,7 +3,16 @@ import React from "react";
 import css from "./Style.module.css";
 
 const VideoPlayer = ({ file }) => (
-  <video className={css.wrapper} width="100%" height="100%" autoPlay muted loop>
+  <video
+    className={css.wrapper}
+    width="100%"
+    height="100%"
+    autoPlay
+    muted
+    loop
+    playsInline
+    preload="auto"
+  >
     <source src={file} type="video/mp4" />
     Ваш браузер не підтримує відео.
   </video>

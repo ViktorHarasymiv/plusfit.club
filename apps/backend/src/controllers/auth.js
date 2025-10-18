f5b3b57c2ce18d5881019d88d3c57c6667123acb
Viktor <victorharasymiv97@gmail.com>
2025-10-18 21:51:54 +0200

    New update (Change sameSite)

--- apps/backend/src/controllers/auth.js
+++ apps/backend/src/controllers/auth.js
@@ -29,10 +29,14 @@ export const loginUserController = async (req, res) => {
 
   res.cookie('refreshToken', session.refreshToken, {
     httpOnly: true,
+    secure: true,
+    sameSite: 'none',
     expires: new Date(Date.now() + ONE_DAY),
   });
   res.cookie('sessionId', session._id, {
     httpOnly: true,
+    secure: true,
+    sameSite: 'none',
     expires: new Date(Date.now() + ONE_DAY),
   });
 

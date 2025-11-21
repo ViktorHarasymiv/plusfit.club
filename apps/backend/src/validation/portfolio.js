aa6046a3427638e1154a80c7ec882933745ff607
Viktor <victorharasymiv97@gmail.com>
2025-11-21 17:20:49 +0100

    New update 17:20

--- apps/backend/src/validation/portfolio.js
+++ apps/backend/src/validation/portfolio.js
@@ -2,14 +2,7 @@ import Joi from 'joi';
 
 export const portfolioValidationSchema = Joi.object({
   section: Joi.string()
-    .valid(
-      'Тренажерний зал',
-      'Масаж',
-      'Реабілітація',
-      'Йога',
-      'Дитячі танці',
-      'Ендосфера',
-    )
+    .valid('Gym', 'Fitness', 'Rehabilitation', 'Yoga', 'Massage')
     .required(),
   alt: Joi.string().min(2).max(100).required(),
 });

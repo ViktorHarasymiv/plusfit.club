a1dcec76061df9d713098820fdecbd02c88f4cf3
Viktor <victorharasymiv97@gmail.com>
2025-11-27 21:49:45 +0100

    New update 21:49

--- apps/backend/src/services/mailer.js
+++ apps/backend/src/services/mailer.js
@@ -8,6 +8,7 @@ import nodemailer from 'nodemailer';
 const transporter = nodemailer.createTransport({
   host: getEnvVar(SMTP.SMTP_HOST),
   port: Number(getEnvVar(SMTP.SMTP_PORT)),
+  secure: false,
   auth: {
     user: getEnvVar(SMTP.SMTP_USER),
     pass: getEnvVar(SMTP.SMTP_PASSWORD),

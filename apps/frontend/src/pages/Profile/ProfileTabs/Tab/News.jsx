ce96cf880a95344993a11c1656c659213f86f75b
Viktor <victorharasymiv97@gmail.com>
2025-12-20 22:28:38 +0100

    New update 22:28

--- apps/frontend/src/pages/Profile/ProfileTabs/Tab/News.jsx
+++ apps/frontend/src/pages/Profile/ProfileTabs/Tab/News.jsx
@@ -1,7 +1,62 @@
-import React from "react";
+import { useEffect } from "react";
+import { usePostStore } from "../../../../store/postStore";
+
+import css from "./Style.module.css";
+import { Link } from "react-router-dom";
+import Button from "../../../../components/ui/Button/Button";
 
 function News() {
-  return <div>News</div>;
+  const { get_post, content, pagination } = usePostStore();
+
+  const perPage = 6;
+  const filterBy = "News";
+
+  useEffect(() => {
+    const fetch_post_data = async () => {
+      await get_post();
+    };
+    fetch_post_data({ perPage, filterBy });
+  }, []);
+
+  console.log(content, pagination);
+
+  return (
+    <div>
+      <ul className={css.list_wrapper}>
+        {content?.map((item, i) => {
+          return (
+            <li className={css.item_wrapper} key={i}>
+              <img
+                className={css.image}
+                src={item.images[0]}
+                alt={`Image ${i}`}
+                width={120}
+                height={80}
+              />
+              <div className={css.content_wrapper}>
+                <h3 className={css.title}>{item.title}</h3>
+                <p className={css.description}>{item.description}</p>
+                <ul className={css.tags_list}>
+                  {item.tags.map((item, i) => {
+                    return (
+                      <li className={css.tag_item} key={i}>
+                        #{item}
+                      </li>
+                    );
+                  })}
+                </ul>
+                <Link to={`/blog/${item._id}`}>
+                  <Button styles={{ maxHeight: "22px", fontSize: "12px" }}>
+                    Get by post
+                  </Button>
+                </Link>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
 
 export default News;
